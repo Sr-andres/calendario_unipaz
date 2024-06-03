b740a6a59be50b8f55c733b85a62037b666e2c68
Mateo <mateo.jimenez@bighealth.com>
2024-06-03 13:03:31 -0500

    Adding prev

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
-app = Flask (__name__)
+app = Flask(__name__)
+
+USER = "andres"
+PASSWORD = "1234"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    is_error = False
+    if (
+        request.method == "POST"
+        and request.form.get("username") == USER
+        and request.form.get("password") == PASSWORD
+    ):
+        return redirect("/")
+    
+    else:
+        is_error = True
+
+    return render_template("/auth/login.html", data={"is_error": is_error})
+
+
+
+@app.route("/login", methods=["GET"])
+def login1():
+    return render_template("/auth/login.html", data={})
 
 @app.route("/")
 def index():
-    return render_template("/auth/login.html")
+    print(request.args.get("data"))
+    return render_template("home.html", data={})
 
-if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
