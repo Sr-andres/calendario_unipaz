4bf526b044b3b897a7d8f5a62b72b20748135844
Sr-andres <andressanabria744@gmail.com>
2024-06-07 10:38:07 -0500

    Adding fake data

--- app.py
+++ app.py
@@ -2,22 +2,38 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-USER = "andres"
-PASSWORD = "1234"
+
+FAKE_DATABASE = [
+    {
+        "user": "andres",
+        "password": "1234"
+    },
+        {
+        "user": "alvaro",
+        "password": "1234"
+    },
+]
 
 
 @app.route("/login", methods=["POST"])
 def login():
-    is_error = False
-    if (
-        request.method == "POST"
-        and request.form.get("username") == USER
-        and request.form.get("password") == PASSWORD
-    ):
-        return redirect("/")
+    is_error = True
+    if request.method == "POST":
+        for data in FAKE_DATABASE:
+            if data["user"] == request.form.get("username") and data["password"] ==  request.form.get("password"):
+                is_error = False
+                return redirect("/")
+
+    # is_error = False
+    # if (
+    #     request.method == "POST"
+    #     and request.form.get("username") == USER
+    #     and request.form.get("password") == PASSWORD
+    # ):
+    #     return redirect("/")
     
-    else:
-        is_error = True
+    # else:
+    #     is_error = True
 
     return render_template("/auth/login.html", data={"is_error": is_error})
 
